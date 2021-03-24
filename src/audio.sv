60dd861713a588b51117f164c90322e525918977
alex <aleksandr.sevcenko@nordsec.com>
2021-03-24 06:15:43 +0200

    ...

--- src/audio.sv
+++ src/audio.sv
@@ -6,25 +6,65 @@ module audio (
   output  out_sclk	, // Serial Data Clock
   output  out_data	);
 
-  bit[32:0] lfsr;
-	bit[8:0]  tick;
+	localparam unsigned last_sample = 87352;
 	
-	assign  out_wclk = tick[8];
-	assign  out_sclk = tick[3];
+	(* ram_init_file = "assets/audio.mif" *)
+	bit[7:0] 	 	samples[0:last_sample];
+	
+	bit[8:0]  	divtick = 0;
+	bit[16:0]		counter = 0;
+	bit[8:0]		current = 0;
+	bit[7:0]		tsample = 0;
+	bit					lastwck	= 0;
+	bit					lastsck	= 0;
+	bit[1:0]		d3count = 0;
+	
   assign  out_mclk = inp_clock;	
-	assign  out_data = lfsr[32];
+	assign  out_sclk = divtick[2];
+	assign  out_wclk = divtick[8];
+	assign  out_data = current[8];
   	
   always @(posedge inp_clock, negedge inp_reset)
   begin
     if (~inp_reset)
-    begin
-      lfsr <= 0;
-			tick <= 0;
+    begin      
+			divtick <= 0;
+			counter <= 0;
+			current <= 0;
+			lastwck <= 0;
+			lastsck	<= 0;
+			d3count <= 0;
+			tsample <= 0;
     end else 
     begin      
-			tick <= tick + 10'b1;
-			if (~out_wclk)
-				lfsr <= {lfsr[31:0], (lfsr[32] ^~ lfsr[22] ^~ lfsr[2] ^~ lfsr[1])};
+			divtick <= divtick + 9'b1;
+			lastwck <= out_wclk;
+			lastsck <= out_sclk;
+			tsample <= samples[counter];
+			
+			
+			if (lastwck != out_wclk)
+			begin
+				current <= tsample;
+			end
+			else if (~lastsck && out_sclk)
+			begin
+				current <= (current << 1);
+			end
+
+			if (~lastwck && out_wclk)
+			begin				
+				if (d3count < 2)
+				begin
+					d3count <= d3count + 1'b1;
+				end	else 
+				begin
+					d3count <= 0;					
+					counter <= counter + 1'b1;				
+					if (counter == last_sample)
+						counter <= 0;
+				end
+			end
     end
   end
  
--- src/video.sv
+++ src/video.sv
@@ -25,57 +25,12 @@ module video (
 	localparam shortint DotVerSyncPulseBeg	= DotVerticalActive + DotVerFrontPorch;
 	localparam shortint DotVerSyncPulseEnd	= DotVerticalActive + DotVerFrontPorch + DotVerSyncPulse;
 		
-	reg [31:0] 	palette [0:255];	
-	reg [7:0] 	bitmap 	[0:76800];
-
-	// Need to latch vsync/hsync because they are
-	bit				  lat_vsync				= 0; 
-	bit				  lat_hsync				= 0; 
-	  
-	bit[31:0]	  dot_horizontal 	= 0;
-	bit[31:0]	  dot_vertical 		= 0;
-	  
-	assign 		  out_clock 			= inp_clock;
-	assign 		  out_vsync				= lat_vsync;
-	assign 		  out_hsync				= lat_hsync;
-	assign 		  out_blank				= (dot_horizontal < DotHorizontalActive) && (dot_vertical < DotVerticalActive);
-		  
-	wire[31:0]  half_pixel_y		= int' (dot_vertical >> 1);
-	wire[31:0]  half_pixel_x		= int' (dot_horizontal >> 1);
-	wire[31:0]  pixel_address		= (half_pixel_y << 8) + (half_pixel_y << 6) + half_pixel_x;
-	  
-	wire[7:0]   pixel_index			= bitmap [pixel_address];
-		  
-	assign 		  out_red 				= palette[pixel_index][7:0];
-	assign 		  out_green 			= palette[pixel_index][15:8];
-	assign 		  out_blue				= palette[pixel_index][23:16];
-			
-	always @(posedge inp_clock, negedge inp_reset)
-	begin
-		if (~inp_reset)
-		begin
-			dot_horizontal 	<= 0;
-			dot_vertical		<= 0;
-		end else 
-		begin
-			dot_horizontal <= dot_horizontal + 1;			
-			if (dot_horizontal >= (DotHorizontalMax - 1))  
-			begin
-				dot_horizontal <= 0;	
-				dot_vertical <= dot_vertical + 1;
-				if (dot_vertical >= (DotVerticalMax - 1))
-				begin
-					dot_vertical <= 0;
-				end
-			end
-			lat_hsync <= 0;
-			lat_vsync <= 0;
-			if ((dot_horizontal < DotHorSyncPulseBeg) || (dot_horizontal >= DotHorSyncPulseEnd))
-				lat_hsync <= 1;
-			if ((dot_vertical < DotVerSyncPulseBeg) || (dot_vertical >= DotVerSyncPulseEnd))
-				lat_vsync <= 1;
-			
-		end
-	end	
+	assign out_red   = 0;
+	assign out_green = 0;
+	assign out_blue  = 0;
+	assign out_hsync = 1;
+	assign out_vsync = 1;
+	assign out_blank = 0;
+	assign out_clock = inp_clock;
 
 endmodule
